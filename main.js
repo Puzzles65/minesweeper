16ea6ba28fb6426277d51b110200214cfea667c7
Puzzles65 <chitrungnguyen12@gmail.com>
2023-05-12 16:30:46 +0100

    randmines

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ function init(){
     // selecting the elements from html 
     const board = document.querySelector('.inner-board');
     // variables
-    const mines = 9;
-    const height = 9;
+    const numMines = 9;
+    const height = 10;
     const width = 10;
     const cellCount = height * width;
     const cells = [];
+
+
+    const minePositions = minesRand(cellCount, numMines);
 
     // create game board
     function createBoard(){
         for(let i=0; i<cellCount; i++){
             const cell = document.createElement('div');
-    
-            cell.innerText = i;
     
             cell.dataset.index = i;
     
@@ -23,20 +24,46 @@ function init(){
             cell.style.width = `${100 / width}%`;
     
             board.appendChild(cell);
-    
             cells.push(cell);
+            
+            if (minePositions[i]) {
+                cell.classList.add('mine');
+              }
         }
     }
     createBoard();
     
-}
-// Functions
-function minesRand(){
+// randomize mines 
+    console.log(cells);
     
 }
-// game logic 
+// Functions
+function minesRand(cellCount, numMines){
+    const minePositions = new Array(cellCount).fill(false);
+
+    for (let i=0; i<numMines; i++) {
+      let randomIndex = Math.floor(Math.random() * cellCount);
+      while (minePositions[randomIndex]) {
+        randomIndex = Math.floor(Math.random() * cellCount);
+      }
+      minePositions[randomIndex] = true;
+    }
+    
+    return minePositions;
+}
+
+
+const mine = element.classList.contains('mine');
 // randomize mines placement on the given board 
-// timer 
+// using Math.random and add it to the boards of divs using array
+
+// game logic 
+// click on the board reveal a number or mine (meaning game over)
+// recursion to scan for mines and adding the numbers to the board indicating 
+// the placement of mine 
+// click on empty space reveal neighboring empty spaces
+
+// adding flag function
 // mines left that value are decresing with each flag 
 // can flag more than existing mines therefore the value after 9 flag is
 // going to (-) negative value
@@ -45,7 +72,10 @@ function minesRand(){
 // for board listening to the right click of player to reveal 
 // for flag listening to the left click of the player to assign flag 
 // flagged element is blocking right clicking option
-// play again button 
+
+// click on bomb, game over and reset board
+// add play again button? 
+// add timer?
 
 // calling init function
 window.addEventListener('DOMContentLoaded', init);
